55bec9f8ad409ca8f33ec38fa372170f3f0a6641
wenjieshen <jay@lottiefiles.com>
2025-11-02 02:42:55 +0800

    Add more test cases

--- test/internal/testGeometry.cpp
+++ test/internal/testGeometry.cpp
@@ -132,7 +132,36 @@ TEST_CASE("RenderPath optimize - Flat Cubic Curve", "[tvgRenderPath]")
     RenderPath::optimize(in, out, matrix);
 
     REQUIRE(out.cmds.count == 2);
+    REQUIRE(out.cmds[0] == PathCommand::MoveTo);
+    REQUIRE(out.cmds[1] == PathCommand::LineTo);
     REQUIRE(out.pts.count == 2);
+    REQUIRE(out.pts[0].x == 0.0f);
+    REQUIRE(out.pts[0].y == 0.0f);
+    REQUIRE(out.pts[1].x == 10.0f);
+    REQUIRE(out.pts[1].y == 10.0f);
+}
+
+TEST_CASE("RenderPath optimize - Flat Cubic Colinear with Line", "[tvgRenderPath]")
+{
+    auto matrix = tvg::identity();
+
+    RenderPath in;
+    // Line from (0,0) to (5,5), then flat cubic from (5,5) to (10,10) on same line y = x
+    in.moveTo({0.0f, 0.0f});
+    in.lineTo({5.0f, 5.0f});
+    in.cubicTo({6.67f, 6.67f}, {8.33f, 8.33f}, {10.0f, 10.0f});
+
+    RenderPath out;
+    RenderPath::optimize(in, out, matrix);
+
+    REQUIRE(out.cmds.count == 2);
+    REQUIRE(out.cmds[0] == PathCommand::MoveTo);
+    REQUIRE(out.cmds[1] == PathCommand::LineTo);
+    REQUIRE(out.pts.count == 2);
+    REQUIRE(out.pts[0].x == 0.0f);
+    REQUIRE(out.pts[0].y == 0.0f);
+    REQUIRE(out.pts[1].x == 10.0f);
+    REQUIRE(out.pts[1].y == 10.0f);
 }
 
 TEST_CASE("RenderPath optimize - Multiple Subpaths", "[tvgRenderPath]")
@@ -142,8 +171,8 @@ TEST_CASE("RenderPath optimize - Multiple Subpaths", "[tvgRenderPath]")
     RenderPath in;
     in.moveTo({0.0f, 0.0f});
     in.lineTo({10.0f, 10.0f});
-    in.moveTo({20.0f, 20.0f});
-    in.lineTo({30.0f, 30.0f});
+    in.moveTo({10.0f, 10.0f});
+    in.lineTo({20.0f, 20.0f});
 
     RenderPath out;
     RenderPath::optimize(in, out, matrix);
@@ -156,7 +185,7 @@ TEST_CASE("RenderPath optimize - Multiple Subpaths", "[tvgRenderPath]")
     REQUIRE(moveToCount == 2);
 }
 
-TEST_CASE("RenderPath optimize - With Transform Matrix", "[tvgRenderPath]")
+TEST_CASE("RenderPath optimize - With Scale Up Transform Matrix", "[tvgRenderPath]")
 {
     auto matrix = Matrix{2.0f, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f, 0.0f, 0.0f, 1.0f};
 
@@ -167,8 +196,27 @@ TEST_CASE("RenderPath optimize - With Transform Matrix", "[tvgRenderPath]")
     RenderPath out;
     RenderPath::optimize(in, out, matrix);
 
-    REQUIRE(out.cmds.count >= 1);
-    REQUIRE(out.pts.count >= 1);
+    REQUIRE(out.cmds.count == 2);
+    REQUIRE(out.cmds[0] == PathCommand::MoveTo);
+    REQUIRE(out.cmds[1] == PathCommand::LineTo);
+    REQUIRE(out.pts.count == 2);
+}
+
+TEST_CASE("RenderPath optimize - With Shrink Transform Matrix", "[tvgRenderPath]")
+{
+    auto matrix = Matrix{0.001f, 0.0f, 0.0f, 0.0f, 0.001f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+    RenderPath in;
+    in.moveTo({0.0f, 0.0f});
+    in.lineTo({10.0f, 10.0f});
+
+    RenderPath out;
+    RenderPath::optimize(in, out, matrix);
+
+
+    REQUIRE(out.cmds.count == 1);
+    REQUIRE(out.cmds[0] == PathCommand::MoveTo);
+    REQUIRE(out.pts.count == 1);
 }
 
 TEST_CASE("RenderPath optimize - Complex Path", "[tvgRenderPath]")
@@ -198,13 +246,11 @@ TEST_CASE("RenderPath optimize - Merge Duplicate CubicTo", "[tvgRenderPath]")
 
     RenderPath in;
     in.moveTo({0.0f, 0.0f});
-    // Cubic curve with end point very close to start
     in.cubicTo({0.05f, 0.0f}, {0.05f, 0.05f}, {0.05f, 0.05f});
 
     RenderPath out;
     RenderPath::optimize(in, out, matrix);
 
-    // Should merge close points
     REQUIRE(out.cmds.count == 1);
     REQUIRE(out.cmds[0] == PathCommand::MoveTo);
 }
@@ -222,8 +268,33 @@ TEST_CASE("RenderPath optimize - Three Collinear Lines", "[tvgRenderPath]")
     RenderPath out;
     RenderPath::optimize(in, out, matrix);
 
-    // Should optimize collinear segments
-    REQUIRE(out.cmds.count >= 1);
+    REQUIRE(out.cmds.count == 2);
     REQUIRE(out.cmds[0] == PathCommand::MoveTo);
+    REQUIRE(out.cmds[1] == PathCommand::LineTo);
+    REQUIRE(out.pts.count == 2);
+    REQUIRE(out.pts[0].x == 0.0f);
+    REQUIRE(out.pts[0].y == 0.0f);
+    REQUIRE(out.pts[1].x == 30.0f);
+    REQUIRE(out.pts[1].y == 0.0f);
+}
+
+TEST_CASE("RenderPath optimize - Flat Cubic with Shrink Matrix", "[tvgRenderPath]")
+{
+    auto matrix = Matrix{0.01f, 0.0f, 0.0f, 0.0f, 0.01f, 0.0f, 0.0f, 0.0f, 1.0f};
+
+    RenderPath in;
+    in.moveTo({0.0f, 0.0f});
+    in.cubicTo({50.0f, 50.0f}, {50.0f, 50.0f}, {100.0f, 100.0f});
+
+    RenderPath out;
+    RenderPath::optimize(in, out, matrix);
+
+    REQUIRE(out.cmds.count == 2);
+    REQUIRE(out.cmds[0] == PathCommand::MoveTo);
+    REQUIRE(out.pts.count == 2);
+    REQUIRE(out.pts[0].x == 0.0f);
+    REQUIRE(out.pts[0].y == 0.0f);
+    REQUIRE(out.pts[1].x == 100.0f);
+    REQUIRE(out.pts[1].y == 100.0f);
 }
 
